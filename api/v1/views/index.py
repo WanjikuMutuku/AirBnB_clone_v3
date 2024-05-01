9bda8978e44da769928d8a305301e00078fe55cd
wanjikumutuku <moreenkinyua4@gmail.com>
2024-04-30 23:22:52 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
+"""api index"""
+
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models import storage
+from models.base_model import BaseModel
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    return jsonify({'status': 'OK'})
+
+@app_views.route('/status', strict_slashes=False)
+def get_stats():
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
