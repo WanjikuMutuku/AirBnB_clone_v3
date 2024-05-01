d0212d31f17c9cf71aa1de1ea60cd94545aa70ac
wanjikumutuku <moreenkinyua4@gmail.com>
2024-04-30 23:30:18 -0700

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """app file of v1"""
 
-import flask from Flask
+import flask from Flask, jsonify
 from storage import models
 from app_views import api.v1.views
 
@@ -12,6 +12,12 @@ app.register_blueprint(app_views, url_prefix= '/api/v1')
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+@app.errorhandler(404)
+def handle_not_found_error(error):
+    response = jsonify({'error': 'Not found'})
+    response.status_code = 404
+    return response
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
